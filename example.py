c3ad449f74dbd4808cc360bbf2faaca22985db5b
Bruce Goldfeder <bgoldfe2@masonlive.gmu.edu>
2017-12-02 14:39:16 -0500

    starting on cvxopt example

---
+++ example.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 14:27:12 2017
+
+@author: Bruce
+"""
+
+import mnist, svmcmpl, cvxopt, random
+
+digits1 = [ 0 ]
+digits2 = [ 1 ]
+
+m1 = 4000; m2 = 4000
+
+# read training data
+images, labels = mnist.read(digits1 + digits2, dataset = "training", path = "data/mnist")
+images = images / 256.
+
+C1 = [ k for k in range(len(labels)) if labels[k] in digits1 ]
+C2 = [ k for k in range(len(labels)) if labels[k] in digits2 ]
+
+random.seed()
+random.shuffle(C1)
+random.shuffle(C2)
+
+train = C1[:m1] + C2[:m2]
+random.shuffle(train)
+X = images[train,:]
+d = cvxopt.matrix([ 2*(k in digits1) - 1 for k in labels[train] ])
+
+gamma = 4.0
+sol = svmcmpl.softmargin_appr(X, d, gamma, width = 50, kernel = 'rbf', sigma = 2**5)
